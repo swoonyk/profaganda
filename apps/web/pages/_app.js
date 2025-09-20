0aeb3ef09508529861d239c047bce92a03ed7599
Clément <clementproze@gmail.com>
2025-09-20 16:23:52 -0400

    add css

---
+++ apps/web/pages/_app.js
@@ -0,0 +1,6 @@
+// apps/web/pages/_app.js
+import "../styles/globals.css"; // adjust path as needed
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,12 +4,22 @@ import React from "react";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Profaganda!</h1>
-      <p>This is your homepage.</p>
-    
-        <Button onClick={() => alert('Button clicked!')}>
-            Click Me
-        </Button>
+      <h1>Profaganda!</h1>
+      <p>
+        Guess which Cornell prof matches the review. Race your friends to the
+        right answer!
+      </p>
+
+      <Button variant="primary" onClick={() => alert("This will join game!")}>
+        Join game
+      </Button>
+
+      <Button
+        variant="secondary"
+        onClick={() => alert("THis will create game!")}
+      >
+        Create game
+      </Button>
     </main>
   );
 }
