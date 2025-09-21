411f8e0163de54084aef312619e76e44ac29c654
Clément <clementproze@gmail.com>
2025-09-20 23:56:01 -0400

    some css changes

--- apps/web/components/ui/Button.tsx
+++ apps/web/components/ui/Button.tsx
@@ -8,98 +8,14 @@ type ButtonProps = {
   variant?: ButtonVariant;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
-const baseStyle: React.CSSProperties = {
-  padding: "0.5rem 2rem",
-  borderRadius: "12px",
-  fontWeight: 500,
-  cursor: "pointer",
-  transition: "120ms ease-out",
-  display: "inline-block",
-  height: "55px",
-  fontSize: "20px",
-};
-
-const hoverStyles: Record<ButtonVariant, React.CSSProperties> = {
-  primary: {
-    backgroundColor: "#09af83ff",
-    boxShadow: "0 10px 0 #007658",
-    transform: "translateY(-4px)",
-  },
-  secondary: {
-    backgroundColor: "#cfd1d6ff",
-    boxShadow: "0 10px 0 #A6A6A6",
-    transform: "translateY(-4px)",
-  },
-  tertiary: {
-    backgroundColor: "#1f4c79ff",
-    boxShadow: "0 10px 0 #082E53",
-    transform: "translateY(-4px)",
-  },
-};
-
-const activeStyles: Record<ButtonVariant, React.CSSProperties> = {
-  primary: {
-    boxShadow: "0 4px 0 #007658",
-    transform: "translateY(2px)",
-  },
-  secondary: {
-    boxShadow: "0 4px 0 #A6A6A6",
-    transform: "translateY(2px)",
-  },
-  tertiary: {
-    boxShadow: "0 4px 0 #082E53",
-    transform: "translateY(2px)",
-  },
-};
-
-const variantStyles: Record<ButtonVariant, React.CSSProperties> = {
-  primary: {
-    backgroundColor: "#0AD6A1",
-    color: "#000",
-    boxShadow: "0 6px 0 #007658",
-  },
-  secondary: {
-    backgroundColor: "#ffffff",
-    color: "#000000",
-    boxShadow: "0 6px 0 #A6A6A6",
-  },
-  tertiary: {
-    backgroundColor: "#2B5D8E",
-    color: "#ffffff",
-    boxShadow: "0 6px 0 #082E53",
-  },
-};
-
 export const Button: React.FC<ButtonProps> = ({
   children,
-  className,
+  className = "",
   variant = "primary",
   ...props
 }) => {
-  const [hover, setHover] = React.useState(false);
-  const [active, setActive] = React.useState(false);
-
-  const style = {
-    ...baseStyle,
-    ...variantStyles[variant],
-    ...(hover ? hoverStyles[variant] : {}),
-    ...(active ? activeStyles[variant] : {}),
-  };
-
   return (
-    <button
-      style={style}
-      className={`${variant} ${className ?? ""}`.trim()}
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => {
-        setHover(false);
-        setActive(false);
-      }}
-      onMouseDown={() => setActive(true)}
-      onMouseUp={() => setActive(false)}
-      onBlur={() => setActive(false)}
-      {...props}
-    >
+    <button className={`btn ${variant} ${className}`.trim()} {...props}>
       {children}
     </button>
   );
