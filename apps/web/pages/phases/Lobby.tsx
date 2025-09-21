31fa05efeee4c0685d892d377386264095c3bc75
Clément <clementproze@gmail.com>
2025-09-21 00:07:44 -0400

    more styles

--- apps/web/pages/phases/Lobby.tsx
+++ apps/web/pages/phases/Lobby.tsx
@@ -19,8 +19,8 @@ export default function Lobby() {
   return (
     <main className="lobby">
       <div className="panel left">
-        <h2>
-          Players ({players.length}/{maxPlayers})
+        <h2 className="player-count">
+          Players ({players.length}/{maxPlayers}):
         </h2>
         <div className="player-list">
           {players.map((p) => (
@@ -53,9 +53,37 @@ export default function Lobby() {
 
         <div className="buttons row">
           <Button variant="secondary" onClick={leaveGame}>
-            Back
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M13 9a1 1 0 0 1-1-1V5.061a1 1 0 0 0-1.811-.75l-6.835 6.836a1.207 1.207 0 0 0 0 1.707l6.835 6.835a1 1 0 0 0 1.811-.75V16a1 1 0 0 1 1-1h6a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1z" />
+            </svg>
+            <span>Back</span>
           </Button>
-          <Button onClick={() => startRound("A")}>Start!</Button>
+          <Button onClick={() => startRound("A")}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z"></path>
+            </svg>
+            <span>Start!</span>
+          </Button>
           {/* change this later to take the variable for the round since its definable */}
         </div>
       </div>
